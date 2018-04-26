a795612eb710dc34cc58f1d4d36799d66e96620e
Gazelle Bahramian <gazellebahramian@gmail.com>
2018-04-26 13:20:15 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+apikey = "jy5gVE4OAByPWWTZL6tFp8CtX"
+weather_apikey = "6022b3a5b3ed50ddac6cb160e76e33d6"
+
+consumer_key = '4oaVujwymb3zGPtw39jaIPCVu'
+consumer_secret = '	fEbVOlv4az4gPQKL4C1dI6860JBpXZqXausdey1ENO3mISvLnW'
+access_token = '969400010327523328-hjqt80cG3UNMyNrRT4CTw1kuqHytB9r'
+access_token_secret ='urpx9bRyeuTpprb6tlPI18ngP8lnKtEdzjITIsvrv9kUQ'
